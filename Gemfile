17cebaf25b195a8271173b42c6bc723eca6f28cd
John Wang <johncwang@gmail.com>
2016-11-29 04:36:47 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source ENV['GEM_SOURCE'] || "https://rubygems.org"
+
+gem 'faraday'
+gem 'ringcentral_sdk'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,31 @@
+#!ruby
+
+require 'faraday'
+require 'sinatra'
+require 'ringcentral_sdk'
+
+# Enter config in .env file
+
+client = RingCentralSdk::REST::Client.new
+config = RingCentralSdk::REST::Config.new.load_dotenv
+client.set_app_config config.app
+client.authorize_user config.user
+
+get '/' do
+  'Simple Media Proxy'
+end
+
+get %r{/(([\w.]+)/restapi/v1.0/account/[\w]+/extension/[\w]+/message-store/[\w]+/content/[\w]+).mp3} do
+  conn = Faraday.new(url: "https://#{params[:captures[1]]}") do |faraday|
+    faraday.response :logger
+    faraday.adapter  Faraday.default_adapter
+  end
+
+  url = "https://#{params[:captures][0]}?access_token=" + client.token.token.to_s
+  res = conn.get url
+  response.headers['Content-Type'] = res.headers['Content-Type']
+  res.body
+end
+
+
+#ringcentral-demo-sm
